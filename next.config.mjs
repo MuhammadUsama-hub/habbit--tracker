052ae6d2afa3a828c72d4b5ff521d6a58f4141c6
MuhammadUsama-hub <muhammadusama798i@gmail.com>
2024-09-20 15:40:28 +0500

    bugfix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+        // Only exclude mongoose on the client-side
+        if (!isServer) {
+          config.resolve.fallback = {
+            fs: false,
+            path: false,
+            os: false,
+            net: false,
+            tls: false,
+            'mongoose': false, // Ensures mongoose is not bundled client-side
+          };
+        }
+}
+}
 
 export default nextConfig;
