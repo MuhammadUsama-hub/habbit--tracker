c87544192e934c7ef89c24bb8e27f373fece23c2
MuhammadUsama-hub <muhammadusama798i@gmail.com>
2024-09-20 15:15:04 +0500

    .

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,1 @@
-'use server'
-
-import { NextResponse } from 'next/server'
-import squareWasm from './square.wasm?module'
- 
-export default async function middleware() {
-  const m = await WebAssembly.instantiate(squareWasm)
-  const answer = m.exports.square(9)
- 
-  const response = NextResponse.next()
-  response.headers.set('x-square', answer.toString())
-  return response
-}
 export { auth as middleware } from './auth'
